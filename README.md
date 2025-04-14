166318695c2b243dde78345be68d8a865473af9c
Juzco <abellakroos04@gmail.com>
2025-04-14 22:23:49 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# BC.Game Mines Web App
+This web app predicts safe tiles using provably fair seeds.
---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>BC.Game Mines Predictor</h1>
+      <p>This app will help you predict safe tiles using provably fair seeds.</p>
+    </div>
+  );
+}
